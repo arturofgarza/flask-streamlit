d63dd49ccbbccf333ff29b1dc2494bf1b6947753
Arturo Fuenlabrada <126019639+arturofgarza@users.noreply.github.com>
2024-11-02 22:21:15 +0000

    correccion

--- src/appstream.py
+++ src/appstream.py
@@ -11,14 +11,14 @@ class_dict={
 
 st.tittle ("Think you have Diabetes?-Test it")
 
-val1= st.slider("Numer of pregnancies had":, min_value = 0, max_value = 10, step=1)
-val2= st.slider("Glucose in blood":, min_value = 10, max_value = 50, step=1)
-val3= st.slider("Blood Pressure":, min_value = 1, max_value = 250, step=1)
-val4= st.slider("Skin Thickness":, min_value = 1, max_value = 60, step=1)
-val5= st.slider("Insuline Level":, min_value = 0, max_value = 250, step=1)
-val6= st.slider("Body Mass Index":, min_value = 0.0, max_value = 100.0, step=0.1)
-val7= st.slider("Diabetes Pedigree Function":, min_value = 0.000, max_value = 1.000, step=0.001)
-val8= st.slider("Age":, min_value = 0, max_value = 100, step=1)
+val1= st.slider("Numer of pregnancies had:", min_value = 0, max_value = 10, step=1)
+val2= st.slider("Glucose in blood:", min_value = 10, max_value = 50, step=1)
+val3= st.slider("Blood Pressure:", min_value = 1, max_value = 250, step=1)
+val4= st.slider("Skin Thickness:", min_value = 1, max_value = 60, step=1)
+val5= st.slider("Insuline Level:", min_value = 0, max_value = 250, step=1)
+val6= st.slider("Body Mass Index:", min_value = 0.0, max_value = 100.0, step=0.1)
+val7= st.slider("Diabetes Pedigree Function:", min_value = 0.000, max_value = 1.000, step=0.001)
+val8= st.slider("Age:", min_value = 0, max_value = 100, step=1)
 
 if st.button("Predict"):
 
