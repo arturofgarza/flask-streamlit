416e36bc5e9ced30d28db101b0d63a9e2620c5af
Arturo Fuenlabrada <126019639+arturofgarza@users.noreply.github.com>
2024-11-02 20:31:58 +0000

    heroku

--- src/appflask.py
+++ src/appflask.py
@@ -6,13 +6,8 @@ import os
 
 app=Flask(__name__)
 
-model_path = os.path.join(os.path.dirname(__file__), '..', 'models', 'BosqueSabio.sav')
+model = load(open("../models/BosqueSabio.sav", "rb"))
 
-# Check if the model file exists
-if not os.path.exists(model_path):
-    print("Model file not found at:", model_path)
-else:
-    model = load(open(model_path, "rb"))
 
 class_dict={
     "0": "Its more probably you're free of Diabetes, still consult a specialist",
