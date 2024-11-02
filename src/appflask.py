832d91eb15a1e6300909ef76d45bc58236841e5c
Arturo Fuenlabrada <126019639+arturofgarza@users.noreply.github.com>
2024-11-02 21:43:18 +0000

    still trying

--- src/appflask.py
+++ src/appflask.py
@@ -17,14 +17,14 @@ class_dict={
 def index():
     if request.method== "POST":
 
-        val1=float(request.form("val1"))
-        val2=float(request.form("val2"))
-        val3=float(request.form("val3"))
-        val4=float(request.form("val4"))
-        val5=float(request.form("val5"))
-        val6=float(request.form("val6"))
-        val7=float(request.form("val7"))
-        val8=float(request.form("val8"))
+        val1=float(request.form["val1"])
+        val2=float(request.form["val2"])
+        val3=float(request.form["val3"])
+        val4=float(request.form["val4"])
+        val5=float(request.form["val5"])
+        val6=float(request.form["val6"])
+        val7=float(request.form["val7"])
+        val8=float(request.form["val8"])
 
         data= [[val1, val2, val3, val4, val5, val6, val7, val8]]
 
